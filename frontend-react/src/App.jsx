a0b17b4cf16f83999573068094831983d9199f5a
shubhamkhr7693 <74674210+shubhamkhr7693@users.noreply.github.com>
2025-08-25 20:01:16 +0530

    homepagereact

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
+import './assets/CSS/style.css'
+import Header from './components/Header'
+import Main from './components/Main'
 
 import './App.css'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Header/>
+    <Main/>
+    <Footer/>
      
     </>
   )
---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <footer className='footer py-3 my-3'></footer>
+    <hr  className='border-bottom'/>
+    <p className='text-light text-center'>&copy; 2025 - Built with 💹 by Shubham Kharsodiya </p>
+    </>
+  )
+}
+
+export default Footer
