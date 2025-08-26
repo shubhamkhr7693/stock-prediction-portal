950e1d8b6219208a1581b09da869bea476f5e8e1
shubhamkhr7693 <74674210+shubhamkhr7693@users.noreply.github.com>
2025-08-27 01:19:56 +0530

    registration page

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,19 +1,30 @@
 import { useState } from 'react'
 import './assets/CSS/style.css'
-import Header from './components/Header'
 import Main from './components/Main'
-
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Register from './components/Register'
+import Header from './components/Header'
 import Footer from './components/Footer'
+import Login from './components/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Header/>
-    <Main/>
-    <Footer/>
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path='/' element={<Main/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/login' element={<Login/>}/>
+    </Routes>
+    <Footer />
+    </BrowserRouter>
+    
+   
+    
      
     </>
   )
